8ad7352a8195d0fb63efce403e428750985a8ad7
Avinash Kumar <iavinas@yandex.com>
2025-02-20 07:41:41 +0530

    training part added, Day 4

--- transformers-from-scratch/train.py
+++ transformers-from-scratch/train.py
@@ -355,4 +355,94 @@ def get_ds(config):
 
     return train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt
 
-def 
+def get_model(config, vocab_src_len, vocal_tgt_len):
+    model = build_transformer(vocab_src_len, vocal_tgt_len, config['seq_len'], config['seq_len'], d_model=config['d_model'])
+    return model
+
+
+def train(config):
+    device = 'cuda' if torch.cuda.is_available() else "mps" if torch.has_mps or torch.backends.mps.is_available() else "cpu"
+    print("Using device:", device)
+
+    device = torch.device(device)
+
+    Path(f"{config['datasource']}_{config['model_folder']}").mkdir(parents=True, exist_ok=True)
+
+    train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt = get_ds(config)
+
+    model = get_model(config, tokenizer_src.get_vocab_size(), tokenizer_tgt.get_vocal_size()).to(device)
+
+    writer = SummaryWriter(config['experiment_name'])
+
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=config['lr'], eps=1e-9)
+
+    initial_epoch = 0
+    global_step = 0
+
+    preload = config['preload']
+
+    model_filename = latest_weights_file_path(config) if preload == 'latest' else get_weights_file_path(config, preload) if preload else None
+    if model_filename:
+        print(f"Preloading model {model_filename}")
+        state = torch.load(model_filename)
+        model.load_state_dict(state['model_state_dict'])
+        initial_epoch = state['epoch'] + 1
+        optimizer.load_state_dict(state['optimizer_state_dict'])
+        global_step = state['global_step']
+    else:
+        print("No model preloaded, starting from scratch")
+    
+    loss_fn = nn.CrossEntropyLoss(ignore_index=tokenizer_src.token_to_id('[PAD]'), label_smoothing=0.1).to(device)
+
+    for epoc in range(initial_epoch, config['num_epochs']):
+        torch.cuda.empty_cache()
+        model.train()
+
+        batch_iterator = tqdm(train_dataloader, desc=f"Processing Epoch {epoc:02d}")
+
+        for batch in batch_iterator:
+            encoder_input = batch['encoder_input '].to(device)
+            decoder_input = batch['decoder_input'].to(device)
+            encoder_mask = batch['encoder_mask'].to(device)
+            decoder_mask = batch['decoder_mask'].to(device)
+            label = batch['label'].to(device)
+
+            encoder_output = model.encode(encoder_input, encoder_mask)
+            decoder_output = model.decode(encoder_output, encoder_mask, decoder_input, decoder_mask)
+            proj_output = model.project(decoder_output)
+
+            loss = loss_fn(proj_output.view(-1, tokenizer_tgt.get_vocab_size()), label.view(-1))
+
+            batch_iterator.set_postfix({
+                "loss" : f"{loss.item():6.3f}"
+            })
+
+            writter.add_scalar('train loss', loss.item(), global_step)
+            writer.flush()
+
+            loss.backward()
+
+            optimizer.step()
+            optimizer.zero_grad(set_to_none=True)
+
+            global_step = global_step + 1
+
+    run_validation(model, val_dataloader, tokenizer_src, tokenizer_tgt, config['seq_len'], device, lambda msg: batch_iterator.write(msg), global_step, writer)
+
+    model_filename = get_weights_file_path(config, f"{epoch:02d}")
+
+    torch.save(
+       {
+        "epoch" : epoch, 
+        "model_state_dict" : model.state_dict(),
+        "optimizer_state_dict" : optimizer.state_dict(),
+        "global_step" : global_step
+       }, model_filename
+
+    )
+
+if __name__ == "__main__":
+    warnings.filterwarnings('ignore')
+    config = get_config()
+    train_model(config)
